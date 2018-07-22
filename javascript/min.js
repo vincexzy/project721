93e29aafca6ae414651af8a9c0d370e68894f736
vincexzy <1091226847@qq.com>
2018-07-22 11:06:44 +0800

    first

---
+++ javascript/min.js
@@ -0,0 +1,12 @@
+$(".nav li:nth-child(1)").click(function () {
+  $(this).parent().children().removeClass("active");
+  $(this).addClass("active");
+});
+$(".nav li:nth-child(2)").click(function () {
+  $(this).parent().children().removeClass("active");
+  $(this).addClass("active");
+});
+$(".nav li:nth-child(3)").click(function () {
+  $(this).parent().children().removeClass("active");
+  $(this).addClass("active");
+});
